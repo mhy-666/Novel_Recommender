1771910e7e167ba564d3952b168562d34eb5e56e
mhy-666 <1950994@tongji.edu.cn>
2024-08-07 02:57:34 -0400

    update frontend and RAG pipepline

---
+++ README.md
@@ -0,0 +1,33 @@
+## Project Goal
+I personally enjoy reading novels and manga comics very much. I believe many people, like me, need personalized recommendations to find content they are interested in. Traditional recommendation systems usually rely on users' historical data and ratings, but these methods may not meet the needs of those who want to explore more detailed/specific subjects or try some new subjects.
+
+This project aims to use the latest LLaMA3 Instruct model, combined with Retrieval-Augmented Generation (RAG) technology, to build a novel/manga recommender. Users can input the novel or manga genres or tags they are interested in, and the system will query the relevant database and generate a personalized recommendation list.
+
+## Timeline(Week)
+1. Investigate the LLaMA3 instruct model and the usage of LLamafile.
+
+2. Collect and preprocess relevant data for novels and manga.
+
+3. Configure and test the LLaMA3 instruct model with LLamafile.
+
+4. Develop and test the backend API with RAG mechanism.
+
+5. Continue backend development and testing.
+
+6. Design and implement the frontend interface.
+
+7. Optimize the frontend based on user feedback.
+
+8. Integrate the frontend and backend systems.
+
+9. Deploy the services to a local server and conduct initial testing.
+
+10. Perform comprehensive testing and optimize the system.
+
+## CI/CD Pipeline
+I choose Github CI/CD as my CI/CD pipeple
+
+## Unit Tests
+In the folder named 'tests', there are unit tests
+
+## Docker container
---
+++ src/data_processing.py
@@ -0,0 +1,45 @@
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
+import os
+from pinecone import Pinecone
+from dotenv import load_dotenv
+
+
+
+def load_data(file_path):
+    loader = CSVLoader(file_path=file_path)
+    data = loader.load()
+    return data
+
+
+def split_text(data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    all_splits = text_splitter.split_documents(data)
+    return all_splits
+
+def set_up_pinecone(index_name = 'novels', namespace = 'novelvector'):
+    # initialize connection to pinecone (get API key at app.pinecone.io)
+    load_dotenv()
+
+    # initialize connection to pinecone (get API key at app.pinecone.io)
+    api_key = os.getenv('PINECONE_API_KEY')
+
+    # configure client
+    pc = Pinecone(api_key=api_key)
+
+    index = pc.Index(index_name)
+    # wait a moment for connection
+
+    index.describe_index_stats()
+    embeddings_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+    vector_store = PineconeVectorStore(index=index, namespace=namespace, embedding=embeddings_model)
+    return vector_store
+
+def retrieve_recommendations(vector_store, query, k=1):
+    results = vector_store.similarity_search_with_score(query, k=k)
+    return results
