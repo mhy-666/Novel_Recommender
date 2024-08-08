effe10277e287ef3c7225176c7c0db2efa0d6792
mhy-666 <2367290636@qq.com>
2024-08-08 00:19:56 -0400

    update requirements

--- src/rag.py
+++ src/rag.py
@@ -1,7 +1,9 @@
 from openai import OpenAI
 import os
+from data_processing import retrieve_recommendations, set_up_pinecone
+from LLMs import create_openai_client, get_book_recommendation
 
-def get_book_recommendation_with_rag(client, genre, description):
+def get_book_recommendation_rag(client, genre, description):
     '''
     Get book recommendations based on user input with rag
     Args:
@@ -11,12 +13,21 @@ def get_book_recommendation_with_rag(client, genre, description):
     Returns:
         response: str, book recommendation
     '''
+    vector_store = set_up_pinecone(index_name = 'novels', namespace = 'novelvector')
+    query = retrieve_recommendations(vector_store, genre, k=1)
+    context = "Here are some book recommendations for preferences:" + query
     messages = [
         {"role": "system", "content": "You are Llama, an AI assistant. Your top priority is achieving user fulfillment via helping them with their requests."},
-        {"role": "user", "content": f"I am looking for a {genre} book. Here are my preferences: {description}"}
+        {"role": "user", "content": f"I am looking for a {genre} book. Here are my preferences: {description}",
+         "role": "assistant", "content": context}
     ]
     completion = client.chat.completions.create(
         model="LLaMA_CPP",
         messages=messages
     )
     return completion.choices[0].message.content
+
+if __name__ == '__main__':
+    client = create_openai_client()
+    result = get_book_recommendation_rag(client, 'fantasy', 'space travel and adventure')
+    print(result)
